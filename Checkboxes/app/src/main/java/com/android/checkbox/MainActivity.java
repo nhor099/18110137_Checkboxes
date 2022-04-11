95a491a00578f23df828358705b88af2e8e48233
DELL <dieuthidiemkieu.l.15cla@gmail.com>
2022-04-11 17:38:37 +0700

    first commit

---
+++ Checkboxes/app/src/main/java/com/android/checkbox/MainActivity.java
@@ -0,0 +1,40 @@
+package com.android.checkbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private String message = "Toppings: ";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toastMessage(View view) {
+        selectToppings((CheckBox) findViewById(R.id.check_syrup), getString(R.string.chocolate_syrup));
+        selectToppings((CheckBox) findViewById(R.id.check_sprinkles), getString(R.string.sprinkles));
+        selectToppings((CheckBox) findViewById(R.id.check_crushed_nuts), getString(R.string.crushed_nuts));
+        selectToppings((CheckBox) findViewById(R.id.check_cherries), getString(R.string.cherries));
+        selectToppings((CheckBox) findViewById(R.id.check_oreo), getString(R.string.orio_cookie_crumbles));
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void selectToppings(CheckBox checkBox, String topping){
+        if (checkBox.isChecked()){
+            if (!message.contains(topping)){
+                message = message + " " + topping;
+            }
+        }
+        else{
+            if (message.contains(topping)){
+                message = message.replace(" " + topping, "");
+            }
+        }
+    }
+}
